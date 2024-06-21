b3cc532af6e60bd6ff2ff3c20c4ff37949744ee7
Armoghan-ul-Mohmin <Armoghan16@gmail.com>
2024-06-21 17:53:28 +0500

    Added Folder Structure and Globals

---
+++ script.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+#
+# Cell Tower Data Analyzer: A Ruby tool for parsing, analyzing, and visualizing data from cell towers, aiding in network optimization and decision-making.
+#
+# Author: Armoghan-ul-Mohmin
+#
+# More info here: https://github.com/Armoghans-Organization/Cell-Tower-Data-Analyzer.git
+#
+# Usage: ruby script.rb [OPTIONS]
+# Example: ruby script.rb --help
+#
+
+##########################################################################
+# Globals
+##########################################################################
+
+# Script Author Name
+$author = "Armoghan-ul-Mohmin"
+# Script Name
+$name = "Cell Tower Data Analyzer"
+# Script Version
+$version = "1.0.0"
+# Script Description
+$description = "Analyzing & Visualizing data from CellTowers"
+# Script URL
+$url = "https://github.com/Armoghans-Organization/Cell-Tower-Data-Analyzer"
